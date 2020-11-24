f26fbeb1dcfe406202afa2a12c51f313587d4674
Samantha <samanthagottlieb31@gmail.com>
2020-11-24 14:59:08 +0000

    Create signup page and welcome page that displays username

--- app.rb
+++ app.rb
@@ -8,11 +8,17 @@ class Makersbnb < Sinatra::Base
   end
 
   get '/signup' do
-
+    erb :signup
   end
 
   post '/signup' do
+    sessions['username'] = params['username']
+    redirect :welcome
+  end
 
+  get '/welcome' do
+    @username = sessions['username']
+    erb :welcome
   end
 
   get '/homes' do
